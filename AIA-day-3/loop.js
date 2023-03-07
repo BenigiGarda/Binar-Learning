c83fcbef84657e8c4d2ac9ed777cc743707360a8
AIA\E128941 <benigi.anvadin@aia.com>
2023-03-07 15:54:13 +0800

    first commit

---
+++ AIA-day-3/loop.js
@@ -0,0 +1,11 @@
+let array = [];
+
+for (let i = 1; i <= 100; i++) {
+  array.push(i);
+}
+
+const filteredArray = array
+  .filter((e) => e % 2 === 0)
+  .filter((e) => (e += 2))
+  .filter((e) => e < 18)
+  .toString();
